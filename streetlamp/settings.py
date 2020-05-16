79f537099588d1bceb409ae2f9ff05fa6e922f52
Prakash Karki <prakashkrk1@gmail.com>
2020-05-17 03:57:17 +1000

    hg

--- streetlamp/settings.py
+++ streetlamp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#m*vsqtcif2$9%1*l)0dspgp3jeq&atz(yo9e)a-%bful-x&io'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['herokustreetlamp.herokuapp.com']
 
 
 # Application definition
